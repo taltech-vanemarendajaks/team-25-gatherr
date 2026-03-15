5645087b5b8a11144e71254e638a6cbbb8b3ab57
taavitaht <taavi656@gmail.com>
2026-03-15 21:59:32 +0200

    feat: backend init

---
+++ backend/src/main/java/com/gatherr/backend/GatherrApplication.java
@@ -0,0 +1,13 @@
+package com.gatherr.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatherrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatherrApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gatherr/backend/GatherrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatherr.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatherrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
