b1504e465e522e1d3b01209c16c11acdb57a4121
tomimarkus991 <tomimarkusalber@gmail.com>
2026-03-25 15:37:34 +0200

    feat: add button component

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -1,61 +1,52 @@
 import { cva, type VariantProps } from "class-variance-authority";
-import { Slot } from "radix-ui";
 import type * as React from "react";
+import { forwardRef } from "react";
 import { cn } from "../../lib/utils";
 
-const buttonVariants = cva(
-	"inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+export const buttonVariantColors = {
+	orange: ["text-content bg-primary border-[#894822]", "active:border-primary"],
+};
+
+export const buttonVariants = cva(
+	[
+		"inline-flex items-center justify-center",
+		"m-0 border-b-[6px] text-center font-medium tracking-wider font-sans whitespace-nowrap",
+		"transition-all duration-300 hover:-translate-y-[0.15rem]",
+		"active:translate-y-[0.2rem] active:duration-75",
+		"touch-manipulation",
+	],
 	{
+		// regular --> hover --> active --> dark --> focus
 		variants: {
-			variant: {
-				default: "bg-primary text-primary-foreground hover:bg-primary/90",
-				destructive:
-					"bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
-				outline:
-					"border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
-				secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-				ghost: "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
-				link: "text-primary underline-offset-4 hover:underline",
-			},
+			variant: buttonVariantColors,
 			size: {
-				default: "h-9 px-4 py-2 has-[>svg]:px-3",
-				xs: "h-6 gap-1 rounded-md px-2 text-xs has-[>svg]:px-1.5 [&_svg:not([class*='size-'])]:size-3",
-				sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-				lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
-				icon: "size-9",
-				"icon-xs": "size-6 rounded-md [&_svg:not([class*='size-'])]:size-3",
-				"icon-sm": "size-8",
-				"icon-lg": "size-10",
+				icon: `py-3 px-5 text-md rounded-lg`,
+				xs: `py-2 px-4 text-sm rounded-xl`,
+				sm: `py-2 px-10 text-sm rounded-xl`,
+				md: `py-2 px-14 text-md rounded-xl`,
+				lg: `py-3 px-18 text-lg rounded-xl`,
+			},
+			isValid: {
+				false: "cursor-not-allowed opacity-50",
 			},
 		},
-		defaultVariants: {
-			variant: "default",
-			size: "default",
-		},
+		defaultVariants: { variant: "orange", size: "md" },
 	},
 );
 
-function Button({
-	className,
-	variant = "default",
-	size = "default",
-	asChild = false,
-	...props
-}: React.ComponentProps<"button"> &
-	VariantProps<typeof buttonVariants> & {
-		asChild?: boolean;
-	}) {
-	const Comp = asChild ? Slot.Root : "button";
+export interface ButtonProps
+	extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+		VariantProps<typeof buttonVariants> {}
 
-	return (
-		<Comp
-			data-slot="button"
-			data-variant={variant}
-			data-size={size}
-			className={cn(buttonVariants({ variant, size, className }))}
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+	({ className, variant, size, type = "button", isValid, ...props }, ref) => (
+		<button
+			type={type}
+			className={cn(buttonVariants({ variant, size, isValid, className }), "")}
+			ref={ref}
 			{...props}
 		/>
-	);
-}
+	),
+);
 
-export { Button, buttonVariants };
+Button.displayName = "Button";
