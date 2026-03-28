3e7ab9ad0b66673192b2e70326827d90d767613d
jsidla <johannastina.idla@gmail.com>
2026-03-28 17:43:43 +0200

    refactor: remove AuthRequest and AuthResponse dtos

--- backend/src/main/java/com/gatherr/backend/dto/AuthResponseDto.java
+++
@@ -1,9 +0,0 @@
-package com.gatherr.backend.dto;
-
-public record AuthResponseDto (
-        String token,
-        Long id,
-        String name,
-        String email,
-        String profilePicture
-) {}
