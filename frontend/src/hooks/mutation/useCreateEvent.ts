bc2d20b759821d9b7b0288bd60e69df7e0203b73
tomimarkus991 <tomimarkusalber@gmail.com>
2026-03-31 10:53:33 +0300

    feat: navigate to event page and show toast on create

--- frontend/src/hooks/mutation/useCreateEvent.ts
+++ frontend/src/hooks/mutation/useCreateEvent.ts
@@ -1,23 +1,33 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-
+import { useNavigate } from "@tanstack/react-router";
+import toast from "react-hot-toast";
 import { GatherrApiClient } from "../../lib/axios";
 import type { Event } from "../../mocks/types";
+import { useGetMe } from "../query/useGetMe";
 
-interface CreateEventInput extends Partial<Event> {}
+interface CreateInput extends Partial<Event> {}
 
 interface ResponseType extends Event {}
 
-const mutationFn = async (input: CreateEventInput): Promise<ResponseType> => {
+const mutationFn = async (input: CreateInput): Promise<ResponseType> => {
 	const { data } = await GatherrApiClient.post<ResponseType>("/events", input);
+
 	return data;
 };
 
 export const useCreateEvent = () => {
 	const queryClient = useQueryClient();
+	const { data: user } = useGetMe();
+	const navigate = useNavigate();
 
 	return useMutation({
 		mutationFn,
-		onSuccess: () => {
+		onSuccess: ({ shortId }) => {
+			if (user && shortId) {
+				toast.success("Event created");
+				console.log(user, shortId);
+				navigate({ to: `/e/${shortId}` });
+			}
 			queryClient.invalidateQueries({ queryKey: ["event"] });
 		},
 	});
