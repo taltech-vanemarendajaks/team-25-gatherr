06e4f05b73a50f297f831a16ece2fd37d977182a
Hannes Verlis <61347431+hannesverlis@users.noreply.github.com>
2026-04-03 23:59:11 +0300

    #25 feat: update homepage layout and styles

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,19 +1,40 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
+import FireplaceIcon from "../components/icons/fireplace";
 
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
 	return (
-		<main className="max-w-md m-auto mt-40 font-logo flex flex-col gap-4">
-			<p className="text-5xl text-center">Gatherr</p>
-			<p className="text-center text-2xl">Paly and gather in seconds.</p>
-			<div className="text-center text-0.5xl">just share the link and </div>
-			<div className="flex flex-col gap-2 font-logo ">
-				<Input placeholder="Event title" className="font-logo text-center text-white" />
-				<Button className="w-full">Create Event</Button>
-			</div>
-		</main>
+		
+			<main className="min-h-screen bg-surface flex flex-col items-center justify-center  *:px-4">
+				<div className="w-[297px] min-h-[94px] mx-auto flex items-center justify-center">
+					<p className="text-center text-2xl">
+						Plan any gathering
+						<br />
+						<span className="text-primary">in seconds.</span>
+					</p>
+				</div>
+				<div className="w-[339px] min-h-[132px] mx-auto text-center">
+					<p className="text-center text-sm font-main text-info">
+						Just share a link and let everyone pick their availability. The
+						simplest way to coordinate with friends, family, or colleagues.
+					</p>
+				</div>
+				<section className="w-[373px] min-h-[392px] rounded-[21px] bg-canvas px-[17px] py-8 flex flex-col gap-8">
+				<FireplaceIcon className="mx-auto h-[62px] w-auto" alt="Fireplace icon" />
+				<p className="text-center text-2xl">Create a new event</p>
+					<div className="w-[339px] mx-auto flex flex-col gap-12">
+						<Input
+							placeholder="e.g. Team meeting, game night, som"
+							className="h-[66px] rounded-[21px] bg-surface text-center text-placeholder font-main border-0"
+						/>
+						<Button className="w-[310px] h-[52px] mx-auto rounded-[10px]">
+							Create event
+						</Button>
+					</div>
+				</section>
+			</main>
 	);
 }
