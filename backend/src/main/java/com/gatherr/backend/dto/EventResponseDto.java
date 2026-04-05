5c992fdfcad0e5cb0fe42cdd31ab9651c6398c55
Ingrid Lepik <ingridlepik@Ingrids-MacBook-Air.local>
2026-04-05 23:33:28 +0300

    fix: changed setter to record, removed setter and unnecessary fields

--- backend/src/main/java/com/gatherr/backend/dto/EventResponseDto.java
+++ backend/src/main/java/com/gatherr/backend/dto/EventResponseDto.java
@@ -2,40 +2,29 @@ package com.gatherr.backend.dto;
 
 import com.gatherr.backend.model.Event;
 import com.gatherr.backend.model.enums.EventType;
-import lombok.Getter;
-import lombok.Setter;
 
-import java.time.OffsetDateTime;
 import java.util.List;
 
-@Getter
-@Setter
-public class EventResponseDto {
-    private Long id;
-    private String name;
-    private String description;
-    private String shortId;
-    private Long creatorId;
-    private EventType type;
-    private List<String> times;
-    private String timezone;
-    private boolean isDeleted;
-    private OffsetDateTime createdAt;
-    private OffsetDateTime updatedAt;
-
+public record EventResponseDto(
+        Long id,
+        String name,
+        String description,
+        String shortId,
+        Long creatorId,
+        EventType type,
+        List<String> times,
+        String timezone
+) {
     public static EventResponseDto from(Event event) {
-        EventResponseDto eventResponseDto = new EventResponseDto();
-        eventResponseDto.setId(event.getId());
-        eventResponseDto.setName(event.getName());
-        eventResponseDto.setDescription(event.getDescription());
-        eventResponseDto.setShortId(event.getShortId());
-        eventResponseDto.setCreatorId(event.getCreator() != null ? event.getCreator().getId() : null);
-        eventResponseDto.setType(event.getType());
-        eventResponseDto.setTimes(event.getTimes());
-        eventResponseDto.setTimezone(event.getTimezone());
-        eventResponseDto.setDeleted(event.isDeleted());
-        eventResponseDto.setCreatedAt(event.getCreatedAt());
-        eventResponseDto.setUpdatedAt(event.getUpdatedAt());
-        return eventResponseDto;
+        return new EventResponseDto(
+                event.getId(),
+                event.getName(),
+                event.getDescription(),
+                event.getShortId(),
+                event.getCreator() != null ? event.getCreator().getId() : null,
+                event.getType(),
+                event.getTimes(),
+                event.getTimezone()
+        );
     }
 }
