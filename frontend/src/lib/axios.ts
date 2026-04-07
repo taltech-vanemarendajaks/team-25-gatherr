2deaa4ab18e4e0d474c691c7c9af71f377f77f04
tomimarkus991 <tomimarkusalber@gmail.com>
2026-04-07 15:56:59 +0300

    fix: api route axios config

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const GatherrApiClient = axios.create({
 	// @todo: uncomment this when api is live
 	// baseURL: import.meta.env.VITE_API_URL,
-	baseURL: `api/v1`,
+	baseURL: `/api/v1`,
 	headers: {
 		"Content-Type": "application/json",
 	},
