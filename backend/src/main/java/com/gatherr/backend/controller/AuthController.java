ce9a0d588600112c34f881f1b628a2a3484bd3fd
jsidla <johannastina.idla@gmail.com>
2026-04-12 17:08:01 +0200

    refactor: create JwtUtils, utilise it in User and EventController, update AuthController to use @RequiredArgsConstructor

--- backend/src/main/java/com/gatherr/backend/controller/AuthController.java
+++ backend/src/main/java/com/gatherr/backend/controller/AuthController.java
@@ -3,19 +3,18 @@ package com.gatherr.backend.controller;
 import com.gatherr.backend.dto.AuthRequestDto;
 import com.gatherr.backend.dto.AuthResponseDto;
 import com.gatherr.backend.service.AuthService;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.oauth2.jwt.JwtException;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
+
 public class AuthController {
-
     private final AuthService authService;
-
-    public AuthController(AuthService authService) {
-        this.authService = authService;
-    }
 
     @PostMapping("/google")
     public ResponseEntity<AuthResponseDto> googleAuth(@RequestBody AuthRequestDto request) {
