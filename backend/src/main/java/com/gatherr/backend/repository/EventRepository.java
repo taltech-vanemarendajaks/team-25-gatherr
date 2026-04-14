76abb86a04779c68e461a5543e7221ba4a6d14b4
jsidla <johannastina.idla@gmail.com>
2026-04-14 08:52:00 +0200

    fix: get all events related to user

--- backend/src/main/java/com/gatherr/backend/repository/EventRepository.java
+++ backend/src/main/java/com/gatherr/backend/repository/EventRepository.java
@@ -2,9 +2,7 @@ package com.gatherr.backend.repository;
 
 import com.gatherr.backend.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
     boolean existsByShortId(String shortId);
-    List<Event> findByCreatorIdAndIsDeletedFalse(Long creatorId);
 }
--- backend/src/main/java/com/gatherr/backend/repository/EventUserRepository.java
+++ backend/src/main/java/com/gatherr/backend/repository/EventUserRepository.java
@@ -2,6 +2,8 @@ package com.gatherr.backend.repository;
 
 import com.gatherr.backend.model.EventUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface EventUserRepository extends JpaRepository<EventUser, Long> {
+    List<EventUser> findByUserIdAndEvent_IsDeletedFalse(Long userId);
 }
