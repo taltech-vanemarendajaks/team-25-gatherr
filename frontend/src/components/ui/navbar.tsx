9b9c0b2e59aa3d9915353bf970f7286721eee1fa
tomimarkus991 <tomimarkusalber@gmail.com>
2026-04-16 17:40:59 +0300

    fix: front page styles

--- frontend/src/components/ui/navbar.tsx
+++ frontend/src/components/ui/navbar.tsx
@@ -1,12 +1,11 @@
-import { FireplaceIcon } from "../icons/FireplaceIcon";
 import { UserButton } from "./UserButton";
 
 export const Navbar = () => {
 	return (
 		<nav className="text-white max-w-lg m-auto">
-			<div className="flex flex-row items-center justify-between mt-6 mx-2">
-				<FireplaceIcon />
-				<span className="font-viking text-5xl">Gatherr</span>
+			<div className="flex flex-row items-center justify-between mt-8 mx-6.5">
+				<img src="/fireplace.svg" alt="Fireplace" className="h-14 sm:h-16" />
+				<span className="font-viking text-4xl sm:text-5xl">Gatherr</span>
 				<UserButton />
 			</div>
 		</nav>
