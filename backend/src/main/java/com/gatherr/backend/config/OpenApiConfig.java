efc5192db3408f1de86251c9829041bc60e623eb
jsidla <johannastina.idla@gmail.com>
2026-04-19 13:35:44 +0200

    feat: add OpenApiConfig for authentication in Swagger UI

---
+++ backend/src/main/java/com/gatherr/backend/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.gatherr.backend.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        
+        return new OpenAPI()
+                // Tells Swagger that all endpoints require this security scheme by default
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                // Defines what the security scheme actually is (HTTP Bearer Token)
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
+    }
+}
