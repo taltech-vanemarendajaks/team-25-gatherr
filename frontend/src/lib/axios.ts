16ee611ffb2915bed2359ac8db06262220a70c66
tomimarkus991 <tomimarkusalber@gmail.com>
2026-04-22 21:24:37 +0300

    feat: add Bearer token interceptor to Axios client

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -6,3 +6,9 @@ export const GatherrApiClient = axios.create({
 		"Content-Type": "application/json",
 	},
 });
+
+GatherrApiClient.interceptors.request.use(config => {
+	const token = localStorage.getItem("token");
+	if (token) config.headers.Authorization = `Bearer ${token}`;
+	return config;
+});
